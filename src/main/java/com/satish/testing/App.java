4ff31ce9a3fee18dfcbc6645db1d65be891e262d
Satish Kumar Kamavaram <satishkamavaram@gmail.com>
2016-03-07 15:41:35 +0530

    Sfd

--- src/main/java/com/satish/testing/App.java
+++ src/main/java/com/satish/testing/App.java
@@ -9,5 +9,9 @@ public class App
     public static void main( String[] args )
     {
       System.out.println("admin");
+      System.out.println("satish");
+      System.out.println("satish1");
+      System.out.println("satish2");
+      System.out.println("satish3");
     }
 }
